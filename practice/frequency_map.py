5696ff5fc20cadf63d5861d600e47766d584089f
DevaNandanJS <devanandan.connect@gmail.com>
2026-05-18 17:27:00 +0530

    method 2 for fequency, hash map

--- practice/frequency_map.py
+++ practice/frequency_map.py
@@ -1,15 +1,12 @@
 nums = [5, 6, 7, 1, 111, 9, 3, 4, 10, 70, 11, 1, 1, 5, 9]
 x= 1
 
-frq= {}
+n= len(nums)
+hash_map= {}
 
-for i in range(0, len(nums)):
-    if nums[i] in frq:
-        frq[nums[i]] += 1
-    else:
-        frq[nums[i]] = 1
+for i in range(0, n):
+    hash_map= hash_map.get(nums[i], 0) + 1
 
-print(f"the frequency of all numbers in the array is: {frq}")
-print(f"frequency of {x} is {frq[x]}")
-
+print(f"the frq of numbers are : {hash_map}")
+print(f"the frq of {x} is {hash_map.get(x, 0)}")
 
